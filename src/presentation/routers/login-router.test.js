26b41efc439753ff06aff640960b9cd918d68992
UT0P1C <ljonham@gmail.com>
2023-01-11 16:29:20 -0300

    LoginRouter return 400 if password is not provided

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -9,13 +9,26 @@ class LoginRouter {
 }
 
 describe('Login Router', () => {
-  test('shoudl return 400 if email is not provided', () => {
+  test('should return 400 if email is not provided', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
         password: 'any_pass'
       }
     }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('should return 400 if password is not provided', () => {
+    const sut = new LoginRouter()
+
+    const httpRequest = {
+      body: {
+        email: 'anyemail@email.com'
+      }
+    }
+
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
   })
